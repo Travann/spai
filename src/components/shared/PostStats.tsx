a30b29e9472ee065f4a926e49d9363dc8cd33b6d
Kun Oracle <ploykampochii@gmail.com>
2023-11-18 15:01:31 +0700

    last

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,6 +1,4 @@
-import { useUserContext } from "@/context/AuthContext";
 import {
-  useDeletePost,
   useDeleteSavedPost,
   useGetCurrentUser,
   useLikePost,
@@ -9,16 +7,15 @@ import {
 import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
 import { useEffect, useState } from "react";
-import { record } from "zod";
 import Loader from "./Loader";
 
 type PostStatsProps = {
-  post: Models.Document;
+  post?: Models.Document;
   userId: string;
 };
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-  const likesList = post.likes.map((user: Models.Document) => user.$id);
+  const likesList = post?.likes.map((user: Models.Document) => user.$id);
 
   const [likes, setLikes] = useState(likesList);
   const [isSaved, setIsSaved] = useState(false);
@@ -31,7 +28,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const { data: currentUser } = useGetCurrentUser();
 
   const savedPostRecord = currentUser?.save.find(
-    (record: Models.Document) => record.post.$id === post.$id
+    (record: Models.Document) => record.post.$id === post?.$id
   );
 
   useEffect(() => {
@@ -52,7 +49,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     }
 
     setLikes(likesArray);
-    likePost({ postId: post.$id, likesArray });
+    likePost({ postId: post?.$id || "", likesArray });
   };
 
   const handleSavePost = (
@@ -65,7 +62,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
       return deleteSavePost(savedPostRecord.$id);
     }
 
-    savePost({ userId: userId, postId: post.$id });
+    savePost({ userId: userId, postId: post?.$id || "" });
     setIsSaved(true);
   };
 
--- src/components/shared/RightSiderbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RightSiderbar = () => {
-  return <div>RightSiderbar</div>;
-};
-
-export default RightSiderbar;
